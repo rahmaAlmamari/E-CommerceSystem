646353d06368f3238ec2981f4180db7c759ad5d2
rahmaAlmamari <alr816125@gmail.com>
2025-09-02 16:27:49 +0400

    Extract ICategoryRepo interface

---
+++ Repositories/ICategoryRepo.cs
@@ -0,0 +1,13 @@
+﻿using E_CommerceSystem.Models;
+
+namespace E_CommerceSystem.Repositories
+{
+    public interface ICategoryRepo
+    {
+        void AddCategory(Category category);
+        IEnumerable<Category> GetAllCategory();
+        Category GetCategoryById(int cid);
+        Category GetCategoryByName(string name);
+        void UpdateCategory(Category category);
+    }
+}
