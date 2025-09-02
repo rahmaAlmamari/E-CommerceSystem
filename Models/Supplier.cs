ac02c85ceaa0169e4b0f505fe00ce1f9895f1149
rahmaAlmamari <alr816125@gmail.com>
2025-09-02 15:44:36 +0400

    Adding Supplier class and add fields to it

---
+++ Models/Supplier.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceSystem.Models
+{
+    public class Supplier
+    {
+        [Key]
+        public int SupplierId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+        ErrorMessage = "Invalid email format.(e.g 'example@gmail.com')")]
+        public string ContactEmail { get; set; }
+        [Required]
+        public string Phone { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
