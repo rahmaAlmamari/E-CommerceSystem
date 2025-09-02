2931ca6a378b3d10811a6451eab1c033ad402641
rahmaAlmamari <alr816125@gmail.com>
2025-09-02 15:44:01 +0400

    Adding Category class and add fields to it

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceSystem.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
