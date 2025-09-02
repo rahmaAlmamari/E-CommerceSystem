5ff6b69c21eda78e28257714e43c33eb34228963
rahmaAlmamari <alr816125@gmail.com>
2025-09-02 16:26:14 +0400

    Create CategoryRepo with all need methods

---
+++ Repositories/CategoryRepo.cs
@@ -0,0 +1,90 @@
+﻿using E_CommerceSystem.Models;
+
+namespace E_CommerceSystem.Repositories
+{
+    public class CategoryRepo : ICategoryRepo
+    {
+        public ApplicationDbContext _context;
+        public CategoryRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Get All Category
+        public IEnumerable<Category> GetAllCategory()
+        {
+            try
+            {
+                return _context.Categories.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Database error: {ex.Message}");
+            }
+        }
+        //Get Category by id
+        public Category GetCategoryById(int cid)
+        {
+            try
+            {
+                return _context.Categories.FirstOrDefault(c => c.CategoryId == cid);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Database error: {ex.Message}");
+            }
+        }
+        //Get Category by name
+        public Category GetCategoryByName(string name)
+        {
+            try
+            {
+                return _context.Categories.FirstOrDefault(c => c.Name == name);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Database error: {ex.Message}");
+            }
+        }
+        //Add new Category
+        public void AddCategory(Category category)
+        {
+            try
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Database error: {ex.Message}");
+            }
+        }
+
+        //Update Category 
+        public void UpdateCategory(Category category)
+        {
+            try
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Database error: {ex.Message}");
+            }
+        }
+
+        //Delete Category
+        //public void DeleteCategory(int cid)
+        //{
+        //    try
+        //    {
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new InvalidOperationException($"Database error: {ex.Message}");
+        //    }
+        //}
+    }
+}
