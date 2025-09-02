15777d7e01936dc329bc5780015e3429e752f533
rahmaAlmamari <alr816125@gmail.com>
2025-09-02 15:48:35 +0400

    Adding DbSet for Category and Supplier also set Unique ContactEmail for Supplier

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -14,12 +14,19 @@ namespace E_CommerceSystem
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderProducts> OrderProducts { get; set; }
         public DbSet<Review> Reviews { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //to set Unique Email for User ...
             modelBuilder.Entity<User>()
                         .HasIndex(u => u.Email)
                         .IsUnique();
+            //to set Unique ContactEmail for Supplier ...
+            modelBuilder.Entity<Supplier>()
+            .HasIndex(s => s.ContactEmail)
+            .IsUnique();
 
         }
     }
