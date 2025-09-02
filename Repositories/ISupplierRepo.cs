b42132dcca8fc945cd5e3765d6614eaba608a681
rahmaAlmamari <alr816125@gmail.com>
2025-09-02 16:38:31 +0400

    Extract ISupplierRepo interface

---
+++ Repositories/ISupplierRepo.cs
@@ -0,0 +1,14 @@
+﻿using E_CommerceSystem.Models;
+
+namespace E_CommerceSystem.Repositories
+{
+    public interface ISupplierRepo
+    {
+        void AddSupplier(Supplier supplier);
+        void DeleteSupplier(int sid);
+        IEnumerable<Supplier> GetAllSupplier();
+        Supplier GetSupplierById(int sid);
+        Supplier GetSupplierByName(string name);
+        void UpdateSupplier(Supplier supplier);
+    }
+}
