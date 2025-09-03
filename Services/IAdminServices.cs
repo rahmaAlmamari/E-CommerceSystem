f897478b77a89298ebc2841bfdfb6cb9840603cf
Basma Suliman <basmaalsinani97@gmail.com>
2025-09-03 13:15:06 +0400

    Add IAdminServices

---
+++ Services/IAdminServices.cs
@@ -0,0 +1,12 @@
+﻿
+namespace E_CommerceSystem.Services
+{
+    public interface IAdminServices
+    {
+        IEnumerable<object> GetBestsellingproducts(int topN);
+        IEnumerable<object> GetMostActiveCustomers(int topN);
+        IEnumerable<object> GetRevenuePerDay();
+        IEnumerable<object> GetRevenuePerMonth();
+        IEnumerable<object> GetTopRatedProducts(int topN);
+    }
+}
