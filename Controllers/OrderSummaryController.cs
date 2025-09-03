ce27d5eed07e474ee2aa21d9fd09fc8e030411e8
Basma Suliman <basmaalsinani97@gmail.com>
2025-09-03 16:19:17 +0400

    add new Controller for summary

---
+++ Controllers/OrderSummaryController.cs
@@ -0,0 +1,32 @@
+﻿using E_CommerceSystem.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_CommerceSystem.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderSummaryController : ControllerBase
+    {
+        private readonly IOrderSummaryService _orderSummaryService;
+
+        public OrderSummaryController(IOrderSummaryService orderSummaryService)
+        {
+            _orderSummaryService = orderSummaryService;
+        }
+        [HttpGet("GetOrderSummary/{orderId}")]
+        public IActionResult GetOrderSummary(int orderId)
+        {
+            try
+            {
+                var summary = _orderSummaryService.GetOrderSummary(orderId);
+                return Ok(summary);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
