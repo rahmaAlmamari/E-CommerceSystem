0d64a45443a34ba08dfd6d195d833cbeebe980fe
Basma Suliman <basmaalsinani97@gmail.com>
2025-09-03 14:46:28 +0400

    Add Order Summary DTO

---
+++ Models/OrderSummaryDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace E_CommerceSystem.Models
+{
+    public class OrderSummaryDTO
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        // User info
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+        // Product details
+        public List<OrderProductDetailDTO> Products { get; set; }
+    }
+}
