0ade317b17788f111865cafaa5e4e337c982ff24
Basma Suliman <basmaalsinani97@gmail.com>
2025-09-03 14:46:13 +0400

    Add Order Product Detail DTO

---
+++ Models/OrderProductDetailDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace E_CommerceSystem.Models
+{
+    public class OrderProductDetailDTO
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; } // Price per unit
+        public decimal TotalPrice { get; set; }
+    }
+}
