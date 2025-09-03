e108d5e461a9ff34936c0ad23193997ccbfd0bfb
rahmaAlmamari <alr816125@gmail.com>
2025-09-03 21:35:26 +0400

    Adding max length to fields

--- Models/RefreshToken.cs
+++ Models/RefreshToken.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace E_CommerceSystem.Models
 {
@@ -6,18 +7,29 @@ namespace E_CommerceSystem.Models
     {
         [Key]
         public int Id { get; set; }
+
+        //[ForeignKey("User")]
         public int UserId { get; set; }
 
-        [Required]
+        [Required, MaxLength(200)]
         public string Token { get; set; } = string.Empty; // Store as random opaque string
         public DateTime Expires { get; set; }
         public DateTime Created { get; set; }
+
+        [MaxLength(45)]
         public string CreatedByIp { get; set; } = string.Empty;
 
         public DateTime? Revoked { get; set; }
+
+        [MaxLength(45)]
         public string? RevokedByIp { get; set; }
+
+        [MaxLength(200)]
         public string? ReplacedByToken { get; set; }// for rotation
+
+        [NotMapped]
         public bool IsActive => Revoked == null && !IsExpired;
+        [NotMapped]
         public bool IsExpired => DateTime.UtcNow >= Expires;
 
         // Navigation
