f6df556b10f627b3dcad371bb9c3cc59baacd6dc
rahmaAlmamari <alr816125@gmail.com>
2025-09-03 21:35:51 +0400

    Adding max length for fields

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -6,12 +6,14 @@ namespace E_CommerceSystem.Models
     {
         [Key]
         public int SupplierId { get; set; }
-        [Required]
+        [Required, MaxLength(150)]
         public string Name { get; set; }
+
+        [Required, MaxLength(256)]
         [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$",
         ErrorMessage = "Invalid email format.(e.g 'example@gmail.com')")]
         public string ContactEmail { get; set; }
-        [Required]
+        [Required, MaxLength(8)]
         public string Phone { get; set; }
 
         public virtual ICollection<Product> Products { get; set; }
