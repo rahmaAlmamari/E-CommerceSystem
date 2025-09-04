62f49935fd3a1a76105b5c80e22df7567ab049b7
rahmaAlmamari <alr816125@gmail.com>
2025-09-04 04:03:17 +0400

    Extract ITokenService

---
+++ Services/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using E_CommerceSystem.Models;
+
+namespace E_CommerceSystem.Services
+{
+    public interface ITokenService
+    {
+        string CreateAccessToken(User user);
+        RefreshToken CreateRefreshToken(string ipAddress, int daysToLive);
+    }
+}
