8722ef8bc95e14807303ec28e87827f3f78ba460
Basma Suliman <basmaalsinani97@gmail.com>
2025-09-04 13:37:35 +0400

    edit add supplier controller

--- Controllers/SupplierController.cs
+++ Controllers/SupplierController.cs
@@ -15,14 +15,14 @@ namespace E_CommerceSystem.Controllers
             _supplierService = supplierService;
         }
 
-        [HttpGet]
+        [HttpGet("GetAllSuppliers")]
         public IActionResult GetAllSuppliers()
         {
             var suppliers = _supplierService.GetAllSuppliers();
             return Ok(suppliers);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("GetSupplierById/{id}")]
         public IActionResult GetSupplierById(int id)
         {
             var supplier = _supplierService.GetSupplierById(id);
@@ -31,7 +31,8 @@ namespace E_CommerceSystem.Controllers
                 return NotFound($"Supplier with ID {id}not found.");
             return Ok(supplier);
         }
-        [HttpPost]
+
+        [HttpPost("GetSupplierByName/{name}")]
         public IActionResult GetSupplierByName(string name)
         {
             var supplier = _supplierService.GetSupplierByName(name);
@@ -40,41 +41,44 @@ namespace E_CommerceSystem.Controllers
             return Ok(supplier);
         }
 
-        [HttpPut]
+        [HttpPut("AddSupplier")]
         public IActionResult AddSupplier([FromBody] Supplier supplier)
         {
             if(supplier==null)
                 return BadRequest("Supplier data is required.");
+
             _supplierService.AddSupplier(supplier);
-            return CreatedAtAction(nameof(GetSupplierById), new { id = supplier.SupplierId }, supplier);
+            return Ok($"Supplier '{supplier.Name}' added successfully with ID {supplier.SupplierId}");
 
         }
 
-        [HttpPut("{id}")]
+        [HttpPut("UpdateSupplier/{id}")]
         public IActionResult UpdateSupplier(int id, [FromBody] Supplier supplier)
         {
             if (supplier == null || supplier.SupplierId != id)
-            {
                 return BadRequest("Supplier data is invalid.");
-            }
+            
             var existingSupplier = _supplierService.GetSupplierById(id);
             if (existingSupplier == null)
                 return NotFound($"Supplier with ID {id} not found.");
+
             //to edit only specific fields
             existingSupplier.Name= supplier.Name;
             existingSupplier.ContactEmail = supplier.ContactEmail;
             existingSupplier.Phone = supplier.Phone;
+
              _supplierService.UpdateSupplier(existingSupplier);
             //to return the updated supplier
-            return Ok(existingSupplier);
+            return Ok($"Supplier '{existingSupplier.Name}' updated successfully.");
         }
 
-        [HttpDelete("{id}")]
+        [HttpDelete("DeleteSupplier/{id}")]
         public IActionResult DeleteSupplier(int id)
         {
             var supplier = _supplierService.GetSupplierById(id);
             if (supplier == null)
                 return NotFound($"Supplier with ID {id} not found.");
+
             _supplierService.DeleteSupplier(id);
             return Ok($"Supplier with ID {id} deleted successfully.");
         }
