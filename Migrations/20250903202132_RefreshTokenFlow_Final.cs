8b801861b52ed0e81cc0c54edb5934f88b2ef472
rahmaAlmamari <alr816125@gmail.com>
2025-09-04 04:05:34 +0400

    Create and run RefreshTokenFlow migration

---
+++ Migrations/20250903202132_RefreshTokenFlow_Final.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_CommerceSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefreshTokenFlow_Final : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
