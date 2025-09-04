cbe7b54c3df286045f3477c3eff4891523f5d299
rahmaAlmamari <alr816125@gmail.com>
2025-09-04 11:59:17 +0400

    Adding AppException class in models folder

---
+++ Models/AppException.cs
@@ -0,0 +1,15 @@
+﻿namespace E_CommerceSystem.Models
+{
+    public class AppException : Exception
+    {
+        public int StatusCode { get; }
+        public string? ErrorCode { get; }
+
+        public AppException(string message, int statusCode = 400, string? errorCode = null)
+            : base(message)
+        {
+            StatusCode = statusCode;
+            ErrorCode = errorCode;
+        }
+    }
+}
