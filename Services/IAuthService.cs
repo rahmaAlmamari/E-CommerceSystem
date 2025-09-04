946aafaa3b9ff32282e0aa429b31b10a3f0abe0f
rahmaAlmamari <alr816125@gmail.com>
2025-09-04 04:02:35 +0400

    Extract IAuthService

---
+++ Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using E_CommerceSystem.Models;
+
+namespace E_CommerceSystem.Services
+{
+    public interface IAuthService
+    {
+        Task<(string accessToken, RefreshToken refreshToken)> RefreshAsync(string refreshToken, string ip);
+        Task RevokeAsync(string refreshToken, string ip);
+        Task<(string accessToken, RefreshToken refreshToken)> SignInAsync(string usernameOrEmail, string password, string ip);
+    }
+}
