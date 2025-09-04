38548bd34058a2191b467817564834eb1f3e08a2
rahmaAlmamari <alr816125@gmail.com>
2025-09-04 04:10:07 +0400

    Adding Roles class

---
+++ Models/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace E_CommerceSystem.Auth
+{
+    public static class Roles
+    {
+        public const string Admin = "admin";
+        public const string Manager = "manager";
+        public const string Customer = "customer";
+    }
+}
