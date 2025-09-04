7e277633eb22abe4e6e7c80dd0cfb8f79a3aa388
rahmaAlmamari <alr816125@gmail.com>
2025-09-04 04:11:12 +0400

    Create and run RoleCheckAndDefault Migration

---
+++ Migrations/20250904000926_RoleCheckAndDefault.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_CommerceSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoleCheckAndDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "customer",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_User_Role",
+                table: "Users",
+                sql: "[Role] IN ('admin','manager','customer')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_User_Role",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldDefaultValue: "customer");
+        }
+    }
+}
